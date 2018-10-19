cb3e947f938544a95d61736e3c74fed01ed41cdf
younger96 <474203566@qq.com>
2018-10-19 14:54:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a47420/youngsense/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a47420.youngsense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
